8ce0b7c06ecdcd9c9bcf1ed1349054e7dbc0dae9
yntymakdev <kubanychevyntymak21@gmail.com>
2025-01-16 01:18:39 +0600

    what

--- src/color/color.controller.ts
+++ src/color/color.controller.ts
@@ -1,7 +1,33 @@
-import { Controller } from '@nestjs/common';
-import { ColorService } from './color.service';
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UsePipes, ValidationPipe } from "@nestjs/common";
+import { Auth } from "src/auth/decorators/auth-decorators";
+import { CurrentUser } from "src/user/decorator/user.decorator";
+import { ColorService } from "./color.service";
 
-@Controller('color')
-export class ColorController {
+@Controller("colors")
+export class StoreController {
   constructor(private readonly colorService: ColorService) {}
+  @Auth()
+  @Get("by-Id/:Id")
+  async getByStoreId(@Param("id") storeId: string, @CurrentUser("id") userId: string) {
+    return this.colorService.getById(storeId, userId);
+  }
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Auth()
+  @Post()
+  async create(@CurrentUser("id") userId: string, @Body() dto: CreateStoreDto) {
+    return this.colorService.create(userId, dto);
+  }
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Auth()
+  @Put(":id")
+  async update(@Param("id") storeId: string, @CurrentUser("id") userId: string, @Body() dto: UpdateStoreDto) {
+    return this.colorService.update(storeId, userId, dto);
+  }
+  @Auth()
+  @Delete(":id")
+  async delete(@Param("id") storeId: string, @CurrentUser("id") userId: string) {
+    return this.colorService.delete(storeId, userId);
+  }
 }
