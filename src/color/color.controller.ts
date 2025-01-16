ad2fd172357e9b866c6b7d07d7314edb1e6da14b
yntymakdev <kubanychevyntymak21@gmail.com>
2025-01-16 16:11:13 +0600

    what

--- src/color/color.controller.ts
+++ src/color/color.controller.ts
@@ -2,20 +2,22 @@ import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UsePipes, Va
 import { Auth } from "src/auth/decorators/auth-decorators";
 import { CurrentUser } from "src/user/decorator/user.decorator";
 import { ColorService } from "./color.service";
+import { CreateStoreDto } from "src/store/create-dto.store";
+import { ColorDto } from "./dto/dto.color";
 
 @Controller("colors")
 export class StoreController {
   constructor(private readonly colorService: ColorService) {}
   @Auth()
   @Get("by-Id/:Id")
-  async getByStoreId(@Param("id") storeId: string, @CurrentUser("id") userId: string) {
-    return this.colorService.getById(storeId, userId);
+  async getByStoreId(@Param("id") id: string) {
+    return this.colorService.getById(id);
   }
   @UsePipes(new ValidationPipe())
   @HttpCode(200)
   @Auth()
-  @Post()
-  async create(@CurrentUser("id") userId: string, @Body() dto: CreateStoreDto) {
+  @Post(":storeId")
+  async create(@Param("id") userId: string, @Body() dto: ColorDto) {
     return this.colorService.create(userId, dto);
   }
   @UsePipes(new ValidationPipe())
@@ -27,7 +29,7 @@ export class StoreController {
   }
   @Auth()
   @Delete(":id")
-  async delete(@Param("id") storeId: string, @CurrentUser("id") userId: string) {
+  async delete(@Param("id") storeId: string) {
     return this.colorService.delete(storeId, userId);
   }
 }
