2a39e322e400429fb00c407eaf76e4e69e7a3b8f
yntymakdev <kubanychevyntymak21@gmail.com>
2025-01-19 12:24:38 +0600

    controler review finish

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,7 +1,36 @@
-import { Controller } from '@nestjs/common';
-import { ReviewService } from './review.service';
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, UsePipes, ValidationPipe } from "@nestjs/common";
+import { ReviewService } from "./review.service";
+import { Auth } from "src/auth/decorators/auth-decorators";
+import { CurrentUser } from "src/user/decorator/user.decorator";
+import { ReviewDto } from "./dto/review-dto";
 
-@Controller('review')
+@Controller("reviews")
 export class ReviewController {
   constructor(private readonly reviewService: ReviewService) {}
+
+  @Auth()
+  @Get("by-storeId/:storeId")
+  async getByStoreId(@Param("storeId") storeId: string) {
+    return this.reviewService.getByStoreId(storeId);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Auth()
+  @Post(":productId/:storeId")
+  async create(
+    @CurrentUser("id") userId: string,
+    @Param("productId") productId: string,
+    @Param("storeId") storeId: string,
+    @Body() dto: ReviewDto
+  ) {
+    return this.reviewService.create(userId, productId, storeId, dto);
+  }
+
+  @HttpCode(200)
+  @Auth()
+  @Delete(":id")
+  async delete(@Param("id") id: string, @CurrentUser("id") userId: string) {
+    return this.reviewService.delete(id, userId);
+  }
 }
--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,4 +1,74 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { PrismaService } from "src/prisma.service";
+import { ProductService } from "src/product/product.service";
+import { ReviewDto } from "./dto/review-dto";
 
 @Injectable()
-export class ReviewService {}
+export class ReviewService {
+  constructor(
+    private prisma: PrismaService,
+    private productService: ProductService
+  ) {}
+
+  async getByStoreId(storeId: string) {
+    return this.prisma.review.findMany({
+      where: {
+        storeId,
+      },
+      include: {
+        user: true,
+      },
+    });
+  }
+
+  async getById(id: string, userId: string) {
+    const review = await this.prisma.review.findUnique({
+      where: {
+        id,
+        userId,
+      },
+      include: {
+        user: true,
+      },
+    });
+
+    if (!review) throw new NotFoundException("Отзыв не найден или вы не являетесь его владельцем");
+
+    return review;
+  }
+
+  async create(userId: string, productId: string, storeId: string, dto: ReviewDto) {
+    await this.productService.getById(productId);
+
+    return this.prisma.review.create({
+      data: {
+        ...dto,
+        product: {
+          connect: {
+            id: productId,
+          },
+        },
+        user: {
+          connect: {
+            id: userId,
+          },
+        },
+        store: {
+          connect: {
+            id: storeId,
+          },
+        },
+      },
+    });
+  }
+
+  async delete(id: string, userId: string) {
+    await this.getById(id, userId);
+
+    return this.prisma.review.delete({
+      where: {
+        id,
+      },
+    });
+  }
+}
