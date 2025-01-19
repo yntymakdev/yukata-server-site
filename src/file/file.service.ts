9400e76e55ed6a8504728f19bd62a4b99c638ca9
yntymakdev <kubanychevyntymak21@gmail.com>
2025-01-19 11:17:13 +0600

    file service added

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -1,8 +1,23 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, UploadedFile } from "@nestjs/common";
+import { path } from "app-root-path";
+import { ensureDir, writeFile, writeFileSync } from "fs-extra";
+import { FileResponse } from "./file.interface";
 
 @Injectable()
 export class FileService {
   async saveFiles(files: Express.Multer.File[], folder: string = `products`) {
-    const upload;
+    const uploadedFolder = `${path}/uploads/${folder}`;
+    await ensureDir(uploadedFolder);
+    const response: FileResponse[] = await Promise.all(
+      files.map(async (file) => {
+        const originalName = `${Date.now()}-${file.originalname}`;
+        await writeFile(`${uploadedFolder}/${originalName}`, file.buffer);
+        return {
+          url: `/uploads/${folder}/${originalName}`,
+          name: originalName,
+        };
+      })
+    );
+    return response;
   }
 }
