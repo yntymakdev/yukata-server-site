228b268c1bb34f81dc467d0900b6a4db7e59e270
yntymakdev <kubanychevyntymak21@gmail.com>
2025-01-19 13:12:09 +0600

    created payment shop in yokasa

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { YooCheckout } from "@a2seven/yoo-checkout";
+import { Injectable } from "@nestjs/common";
 
+const checkout = new YooCheckout({
+  shopId: process.env["YOOKASSA_SHOP_ID"],
+  secretKey: process.env["YOOKASSA_CE"],
+});
 @Injectable()
 export class OrderService {}
