fbabd2fe33e9b6dd541220335a8064b547ab0126
yntymakdev <kubanychevyntymak21@gmail.com>
2025-01-19 11:36:32 +0600

    product / service

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { stringify } from "querystring";
+import { PrismaService } from "src/prisma.service";
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(private prisma: PrismaService) {}
+  async getAll(searchTerm?: string) {
+    if (searchTerm) return this.getSearchTermFilter(searchTerm);
+    const product = await this.prisma.product.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+      include: {
+        category: true,
+        color: true,
+        reviews: true,
+      },
+    });
+    return product;
+  }
+}
